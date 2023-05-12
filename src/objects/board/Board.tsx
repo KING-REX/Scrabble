f2454290719ccb5c1e163fd3e1e93a5c8576c75b
KING-REX <anijimjamesmaurice@gmail.com>
2023-05-12 18:46:48 +0100

    Rex's Commit

--- src/objects/board/Board.tsx
+++ src/objects/board/Board.tsx
@@ -2,7 +2,21 @@ import Square from '../square/Square';
 
 class Board {
 
-    // private __squares: Square[][];
+    private __squares: Square[][];
+
+    constructor(squares?: Square[][], colLength?: number, rowLength?: number) {
+        this.__squares = squares ?? this.populateBoard(colLength, rowLength);
+    }
+
+    public populateBoard(colLength?: number, rowLength?: number): Square[][] {
+        let squareArray: Square[][] = [];
+        for(let i = 0; i < (colLength ?? 15); i++) {
+            for(let j = 0; j < (rowLength ?? 15); j++) {
+                squareArray[i][j] = new Square();
+            }
+        }
+        return squareArray;
+    }
 }
 
 export default Board;
